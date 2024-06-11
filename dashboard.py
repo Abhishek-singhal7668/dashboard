b160b80ee50540b497d2b7a75f95aba785fc2ace
Abhishek-singhal7668 <agarwalabhishek7409@gmail.com>
2024-06-11 18:36:37 +0530

    changes in port

--- dashboard.py
+++ dashboard.py
@@ -108,4 +108,5 @@ def update_charts(selected_properties, selected_otas, selected_months):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run_server(debug=True, host='0.0.0.0', port=port)
